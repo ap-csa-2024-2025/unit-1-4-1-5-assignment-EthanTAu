01a23a421bbc2964fd9f16a71187ef0b76fec372
EthanTAu <etau100@student.hbuhsd.edu>
2024-09-09 14:24:32 -0700

    Update Problem0.java

--- Problem0.java
+++ Problem0.java
@@ -2,6 +2,13 @@ public class Problem0
 {
   public static void main(String[] args)
   {
-    // insert code here
+    double initial = 12;
+    System.out.println(initial);
+
+    int second = 12.01;
+    System.out.println(second);
   }
 }
+
+//You are abel to store an int in a double since the double has enough spce to hold the int
+//You cannot store a double in an int because int doesn't have enough spaces for a double
