769e278a0f50856899bcd33be8305c1239052520
EthanTAu <etau100@student.hbuhsd.edu>
2024-09-10 00:39:08 -0700

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,22 @@
 
+public class Problem0
+{
+  public static void main(String[] args)
+  {
+    int initial = 5;
+    System.out.println("Initial value: " + x);
+    x++;
+    System.out.println("number is now " + x);
+    x++;
+    System.out.println("number is now " + x);
+    x++; 
+    System.out.println("number is now " + x);
+    x++;
+    System.out.println("number is now " + x);
+    x--;
+    System.out.println("number is now " + x);
+  }
+}
+
+//You are abel to store an int in a double since the double has enough spce to hold the int
+//You cannot store a double in an int because int doesn't have enough spaces for a double
