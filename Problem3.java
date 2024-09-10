2b29c84cffb422baee2a56b17d111ee91033c4f9
EthanTAu <etau100@student.hbuhsd.edu>
2024-09-10 11:36:23 -0700

    at home

--- Problem3.java
+++ Problem3.java
@@ -3,6 +3,17 @@ public class problem3
     public static void main(Strong[] args);
     {
         final int num = 5814;
-        int first = 
+        int first = num / 1000;
+        int last = num % 10;
+        int third = ((num % 100) - last) / 10;
+        int second = num - first - third - last;
+
+        System.out.println("Your four digit number: " + num);
+        System.out.println(" ");
+        System.out.println("Your four digits in reverse order: ");
+        System.out.println(last);
+        System.out.println(third);
+        System.out.println(second);
+        System.out.println(first);
     }
 }
