e49596450494fccfda940e21a9823440f4c94544
EthanTAu <etau100@student.hbuhsd.edu>
2024-09-11 10:45:16 -0700

    boi

--- Problem3.java
+++ Problem3.java
@@ -1,12 +1,12 @@
 public class problem3
 {
-    public static void main(Strong[] args);
+    public static void main(String[] args)
     {
         final int num = 5814;
         int first = num / 1000;
         int last = num % 10;
         int third = ((num % 100) - last) / 10;
-        int second = num - first - third - last;
+        int second = (num - (first * 1000) - (third * 10) - last) / 100;
 
         System.out.println("Your four digit number: " + num);
         System.out.println(" ");
